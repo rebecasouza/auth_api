797d403230cef021a00160f1d4b551e8f25e1008
rebeca souza <rebeca_souza85@hotmail.com>
2016-07-06 18:14:37 -0300

    adding user authentication

---
+++ app/commands/authenticate_user.rb
@@ -0,0 +1,27 @@
+class AuthenticateUser
+  prepend SimpleCommand
+
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  def call
+    JsonWebToken.encode(user_id: user.id) if user
+  end
+
+  private
+
+  attr_accessor :email, :password
+
+  # Uses the credentials to check if user exists
+  # Authenticates existing user with built-in authenticate method
+  # available via has_secure_password in the User model
+  def user
+    user = User.find_by_email(email)
+    return user if user && user.authenticate(password)
+
+    erros.add :user_authentication, 'invalid credentials'
+    nil
+  end
+end
